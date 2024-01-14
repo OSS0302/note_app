80ac012ec7919e92d5bd4a266416bfec65e65324
OSS0302 <tkdtn3264@gmail.com>
2024-01-14 20:59:30 +0900

    note 노트db 를 NoteRepositoryImpl 연결하기

--- lib/data/repository/note_reposioty_impl.dart
+++ lib/data/repository/note_reposioty_impl.dart
@@ -1,35 +1,36 @@
+import 'package:note_app/data/data_source/note_db.dart';
 import 'package:note_app/domain/model/note.dart';
 import 'package:note_app/domain/repository/note_repository.dart';
 
 class NoteRepositoryImpl implements NoteRepository {
+  final NoteDB db;
+
+  const NoteRepositoryImpl({
+    required this.db,
+  });
+
   @override
-  Future<void> addNote(Note note) {
-    // TODO: implement addNote
-    throw UnimplementedError();
+  Future<void> addNote(Note note) async {
+    await db.addNote(note);
   }
 
   @override
-  Future<void> deleteNote(Note note) {
-    // TODO: implement deleteNote
-    throw UnimplementedError();
+  Future<void> deleteNote(Note note) async {
+    await db.deleteNote(note);
   }
 
   @override
-  Future<Note> getNoteId(int id) {
-    // TODO: implement getNoteId
-    throw UnimplementedError();
+  Future<Note?> getNoteId(int id) async {
+    return await db.getNoteById(id);
   }
 
   @override
-  Future<List<Note>> getNotes() {
-    // TODO: implement getNotes
-    throw UnimplementedError();
+  Future<List<Note>> getNotes() async {
+    return await db.getNotes();
   }
 
   @override
-  Future<void> updateNote(Note note) {
-    // TODO: implement updateNote
-    throw UnimplementedError();
+  Future<void> updateNote(Note note) async {
+    await db.updateNote(note);
   }
-
 }
--- lib/domain/repository/note_repository.dart
+++ lib/domain/repository/note_repository.dart
@@ -4,7 +4,7 @@ abstract interface class NoteRepository {
   Future<List<Note>> getNotes();
 
   // 노트 눌렀을때  id 로 상세화면
-  Future<Note> getNoteId(int id);
+  Future<Note?> getNoteId(int id);
 
   // 노트 추가하기
   Future<void> addNote(Note note);
