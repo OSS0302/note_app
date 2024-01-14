45e5877b78b595ec8823257c9487acff2ce661b0
OSS0302 <tkdtn3264@gmail.com>
2024-01-14 19:52:54 +0900

    노트 repository 작성

---
+++ lib/domain/repository/note_repository.dart
@@ -0,0 +1,17 @@
+import 'package:note_app/domain/model/note.dart';
+
+abstract interface class NoteRepository {
+  Future<List<Note>> getNotes();
+
+  // 노트 눌렀을때  id 로 상세화면
+  Future<Note> getNoteId(int id);
+
+  // 노트 추가하기
+  Future<void> addNote(Note note);
+
+  // 노트 수정하기
+  Future<void> updateNote(Note note);
+
+  // 노트 삭제하기
+  Future<void> deleteNote(Note note);
+}
