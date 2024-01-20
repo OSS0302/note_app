6eb7242e534b3f66342708cd49158f0361067259
OSS0302 <tkdtn3264@gmail.com>
2024-01-20 20:59:44 +0900

    notes 디비 변수명 변경

--- lib/data/repository/note_reposioty_impl.dart
+++ lib/data/repository/note_reposioty_impl.dart
@@ -1,9 +1,9 @@
-import 'package:note_app/data/data_source/note_db.dart';
+import 'package:note_app/data/data_source/note_db_helper.dart';
 import 'package:note_app/domain/model/note.dart';
 import 'package:note_app/domain/repository/note_repository.dart';
 
 class NoteRepositoryImpl implements NoteRepository {
-  final NoteDB db;
+  final NoteDbHelper db;
 
   const NoteRepositoryImpl({
     required this.db,
