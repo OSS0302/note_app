69da92e4afd15c6fc88263f575282b7768dc0ec5
OSS0302 <tkdtn3264@gmail.com>
2024-01-21 21:50:38 +0900

    노트 컬러 선택하는 위젯 생성

---
+++ lib/presentation/colors.dart
@@ -0,0 +1,13 @@
+ import 'dart:ui';
+
+const green = Color(0xFF2E7D32);
+const teal = Color(0xFF004D40);
+const blue = Color(0xFF01579B);
+const amber = Color(0xFFFFA000);
+const lime = Color(0xFFAEEA00);
+
+
+const darkGray = Color(0xFF202020);
+const lightBlue = Color(0xFFD7E8DE);
+
+
